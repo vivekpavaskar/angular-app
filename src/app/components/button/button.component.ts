de6d8dd945fc71c4c0b4aca21a1c4bfb335f2623
Vivek Pavaskar <vivek.pavaskar@gmail.com>
2022-04-08 19:30:04 +0530

    button added with color

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
+  @Input() text!: string;
+  @Input() color!: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
