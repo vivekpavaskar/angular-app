0c42b8604c32c945013724f0ca38053b482bb979
Vivek Pavaskar <vivek.pavaskar@gmail.com>
2022-04-09 14:26:31 +0530

    task component added

--- src/app/components/tasks/tasks.component.spec.ts
+++ src/app/components/tasks/tasks.component.spec.ts
@@ -2,15 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TasksComponent } from './tasks.component';
 
+
 describe('TasksComponent', () => {
   let component: TasksComponent;
   let fixture: ComponentFixture<TasksComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TasksComponent ]
+      declarations: [TasksComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
